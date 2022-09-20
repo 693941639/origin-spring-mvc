624719e98d53c46691d930f470db27db978d4673
Zhao Ji <693941639@qq.com>
2022-09-20 23:01:24 +0800

    datasource

--- src/main/java/com/local/use/AppConfig.java
+++ src/main/java/com/local/use/AppConfig.java
@@ -1,12 +1,40 @@
 package com.local.use;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @ComponentScan
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@PropertySource("classpath:/app.properties")
 public class AppConfig {
+    @Value("${jdbc.url}")
+    String jdbcUrl;
 
+    @Value("${jdbc.username}")
+    String jdbcUsername;
+
+    @Value("${jdbc.password}")
+    String jdbcPassword;
+
+    @Bean
+    DataSource createDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.setJdbcUrl(jdbcUrl);
+        hikariConfig.setUsername(jdbcUsername);
+        hikariConfig.setPassword(jdbcPassword);
+
+        return new HikariDataSource(hikariConfig);
+    }
 }
